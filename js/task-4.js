812218b7370be4efa111bac6831a133818b13321
kostiantyn_ivanchenko <kokos2702@gmail.com>
2024-09-06 03:03:32 +0200

    extr

--- js/task-4.js
+++ js/task-4.js
@@ -4,13 +4,13 @@ getShippingCost(country){
             return ' Shipping to China will cost 100 credits ';
             break;
         case "Chile":
-            return ' Shipping to Chile will cost 250 credits ';
+            return "Shipping to Chile will cost 250 credits" ;
             break;
         case "Australia":
-            return ' Shipping to Australia will cost 170 credits ';
+            return "Shipping to Australia will cost 170 credits";
             break;
         case "Jamaica":
-            return ' Shipping to Jamaica will cost 120 credits ';
+            return "Shipping to Jamaica will cost 120 credits"  ;
             break;
         default:
             return "Sorry, there is no delivery to your country";      
